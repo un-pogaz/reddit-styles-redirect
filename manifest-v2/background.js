f3f60bad87e7c7875faa80eb6e6a0c4514d40320
un-pogaz <46523284+un-pogaz@users.noreply.github.com>
2024-06-04 07:43:22 +0200

    fix short sharing link for user post

--- manifest-v2/background.js
+++ manifest-v2/background.js
@@ -45,7 +45,7 @@ const excludedPaths = [
   /^\/community-points/,
   /^\/r\/[a-zA-Z0-9_]+\/s\/.*/, // eg https://reddit.com/r/comics/s/TjDGhcl22d
   /^\/appeals?/,
-  /\/r\/.*\/s\//,
+  /\/(r|u|user)\/.*\/s\//,
   /\.json$/,
   /\.rss$/,
 ];
